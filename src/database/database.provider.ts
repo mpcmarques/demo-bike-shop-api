49e9fc969fd03d6d66d7b7e38da7566d132aa2b6
mpcmarques <mpcmarques@icloud.com>
2025-06-07 21:24:58 +0100

    db_url

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,9 +1,10 @@
 import * as mongoose from 'mongoose';
 
+const DB_URL = process.env.DB_URL || 'mongodb://localhost/nest';
+
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
-    useFactory: (): Promise<typeof mongoose> =>
-      mongoose.connect('mongodb://localhost/nest'),
+    useFactory: (): Promise<typeof mongoose> => mongoose.connect(DB_URL),
   },
 ];
