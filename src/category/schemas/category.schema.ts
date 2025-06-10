a34540a2ea93bddd70d85ea2c76b779b4a9605fc
mpcmarques <mpcmarques@icloud.com>
2025-06-10 12:06:53 +0100

    show in menu categories

--- src/category/schemas/category.schema.ts
+++ src/category/schemas/category.schema.ts
@@ -4,6 +4,7 @@ export const CategorySchema = new mongoose.Schema({
   name: { type: String, required: true },
   label: { type: String, required: true },
   description: { type: String, required: true },
+  showInMenu: { type: Boolean, default: false },
 });
 
 CategorySchema.index({ label: 'text' });
